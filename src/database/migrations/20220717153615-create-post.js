f20e470aa4bbf1bb0c988097745ebdaf44691850
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-17 12:48:36 -0300

    feat: add post migration

---
+++ src/database/migrations/20220717153615-create-post.js
@@ -0,0 +1,43 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      author: {
+        allowNull: false,
+        field: 'author',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts');
+  }
+};
