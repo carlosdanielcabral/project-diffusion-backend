8b76d7c3d1735be628ed0bd2de1949d39a285628
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-17 12:34:52 -0300

    feat: add User migration

---
+++ src/database/migrations/20220717151046-create-user.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize/types");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
