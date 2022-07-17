c821f6a60c3844ba0d050723fe2caa2e3885d279
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-17 12:32:44 -0300

    refactor: change to typescript

---
+++ src/database/config/config.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import { Options } from 'sequelize';
+
+dotenv.config();
+
+const config: Options = {
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'diffusion',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: Number(process.env.MYSQL_PORT) || 3306,
+  dialect: 'mysql',
+};
+
+module.exports = config;
