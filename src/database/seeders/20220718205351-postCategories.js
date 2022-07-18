e1f2424487064dd201368f4939941d096385f757
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-18 17:59:06 -0300

    feat: add postCategories seed

---
+++ src/database/seeders/20220718205351-postCategories.js
@@ -0,0 +1,24 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('post_categories', [{
+      post_id: 1,
+      category_id: 2
+    }, {
+      post_id: 1,
+      category_id: 2
+    }, {
+      post_id: 2,
+      category_id: 4
+    }, {
+      post_id: 2,
+      category_id: 6
+    }, {
+      post_id: 2,
+      category_id: 7
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('post_categories', null, {});
+  }
+};
