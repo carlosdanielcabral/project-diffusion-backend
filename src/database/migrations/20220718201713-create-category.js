dcdd111338cf78bcad425d5c805de4b05a529e20
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-18 17:38:46 -0300

    fix: change table name

--- src/database/migrations/20220718201713-create-category.js
+++ src/database/migrations/20220718201713-create-category.js
@@ -1,6 +1,6 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Categories', {
+    await queryInterface.createTable('categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -14,6 +14,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Categories');
+    await queryInterface.dropTable('categories');
   }
 };
