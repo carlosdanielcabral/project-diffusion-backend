201486f6c66436d36d8cba72701a1c2245497d5a
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-19 20:08:35 -0300

    feat: add user service test

---
+++ src/__tests__/mocks/user.ts
@@ -0,0 +1,6 @@
+export const createdUser = {
+  id: 1,
+  name: 'User 01',
+  email: 'user01@email.com',
+  password: 'user01'
+};
---
+++ src/__tests__/services/User.spec.ts
@@ -0,0 +1,33 @@
+import chai, { expect } from 'chai';
+import { equal } from 'joi';
+import Sinon from 'sinon';
+import User from '../../database/models/User';
+import ErrorHandler from '../../helpers/ErrorHandler';
+import UserService from '../../services/UserService';
+import { createdUser } from '../mocks/user';
+
+describe('Testa o service User', () => {
+  it('01) Verifica se é possível salvar um usuário', async () => {
+    const service = new UserService(User);
+    Sinon.stub(User, 'findOne').resolves();
+    Sinon.stub(User, 'create').resolves(createdUser as User);
+    const { id, ...user } = createdUser;
+    const response = await service.save(user);
+    expect(response).to.be.equals(createdUser);
+    (User.findOne as Sinon.SinonStub).restore();
+    (User.create as Sinon.SinonStub).restore();
+  });
+
+  it('02) Verifica se não é possível salvar um usuário se o email já existe no banco',
+  async () => {
+    const service = new UserService(User);
+    Sinon.stub(User, 'findOne').resolves(createdUser as User);
+    const { id, ...user } = createdUser;
+    try {
+      await service.save(user);
+    } catch (err) {
+      expect(err.message).to.be.equal('Email already exists');
+    }
+    (User.findOne as Sinon.SinonStub).restore();
+  })
+});
