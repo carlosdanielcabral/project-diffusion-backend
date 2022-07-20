8c15780addf3811b7e693d90cd6dc6001f39872c
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-20 18:46:05 -0300

    feat: add UserFactory and RouteFactory

---
+++ src/factories/RouteFactory.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import UserController from "../controllers/UserController";
+import User from "../database/models/User";
+import UserRouter from "../routes/UserRouter";
+import UserService from "../services/UserService";
+import Route from '../routes';
+
+const RouteFactory = () => {
+  const service = new UserService(User)
+  const controller = new UserController(service);
+  const router = new UserRouter(Router(), controller);
+  const route = new Route(Router(), router);
+  return route;
+};
+
+export default RouteFactory;
---
+++ src/factories/UserFactory.ts
@@ -0,0 +1,11 @@
+import UserController from '../controllers/UserController';
+import User from '../database/models/User';
+import UserService from '../services/UserService';
+
+const UserFactory = () => {
+  const service = new UserService(User);
+  const controller = new UserController(service);
+  return controller;
+}
+
+export default UserFactory;
