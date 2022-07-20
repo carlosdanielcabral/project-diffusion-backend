f710955d24702d29e1a0f5b72e92c1cf303204cd
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-20 18:43:36 -0300

    feat: add routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
+import RouteFactory from './factories/RouteFactory';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
 
 class App {
-  private _app = express();
+  private _app: Express;
+
+  constructor() {
+    this._app = express();
+
+    this._app.get('/', (req, res) => res.status(200).json({ runnig: true }))
+    this.config();
+  }
+
+  get app() {
+    return this._app;
+  }
 
   config = () => {
     this._app.use(express.json());
     this._app.use(cors());
+    const route = RouteFactory();
+    this._app.use('/', route.router);
+    this._app.use(ErrorMiddleware);
   };
 
   listen = (port: number) => 
