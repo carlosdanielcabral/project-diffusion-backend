00e138ad659407dbf45e6a067ddbab7a4cceddd5
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-20 21:23:56 -0300

    feat: add name validation tests

--- src/__tests__/controllers/User.spec.ts
+++ src/__tests__/controllers/User.spec.ts
@@ -14,7 +14,7 @@ describe('Testa o controller User', () => {
   const app = new App();
   const service = new UserService(User);
 
-  it('01) Verifica se é possível salvar um usuário corretamente', async () => {
+  it('01) Verifica se é possível salvar uma pessoa usuária com dados válidos', async () => {
     Sinon.stub(User, 'findOne').resolves();
     Sinon.stub(User, 'create').resolves(createdUser as User);
     Sinon.stub(service, 'save').resolves(createdUser as User);
@@ -24,5 +24,24 @@ describe('Testa o controller User', () => {
     expect(response.body).to.be.deep.equal(createdUser);
     (User.findOne as Sinon.SinonStub).restore();
     (User.create as Sinon.SinonStub).restore();
+  });
+
+  it('02) Verifica se não é possível salvar uma pessoa usuária sem fornecer nome', async () => {
+    const { id, name, ...userData } = createdUser;
+    const response = await chai.request(app.app).post('/user').send(userData);
+    expect(response).to.have.status(400);
+    expect(response.body).to.be.deep.equal({
+      message: '"name" is required',
+    });
+  });
+
+  it('03) Verifica se não é possível salvar uma pessoa usuária com nome inválido', async () => {
+    const { id, ...userData } = createdUser;
+    userData.name = 'ad';
+    const response = await chai.request(app.app).post('/user').send(userData);
+    expect(response).to.have.status(400);
+    expect(response.body).to.be.deep.equal({
+      message: '"name" length must be at least 3 characters long',
+    });
   });
 });
