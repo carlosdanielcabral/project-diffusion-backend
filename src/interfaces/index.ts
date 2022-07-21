dd0d32f9d9628acbe6d40a42b9882e12545b3b10
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-21 14:10:10 -0300

    feat: add update method to IUserService

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -9,6 +9,7 @@ export interface IUserService {
   findOne: (field: TUserField, value: number | string) => Promise<TUser>;
   login: (data: TUser) => Promise<TUser>;
   save: (data: TUser) => Promise<TUser>;
+  update: (data: TUser) => Promise<TUser>;
 }
 
 export interface IErrorHandler extends Error {
