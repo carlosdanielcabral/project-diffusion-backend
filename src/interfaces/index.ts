bab8db2f808c8bbf1af0e8543e35efff81e91055
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-20 21:23:07 -0300

    feat: add interface IErrorHandler

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,11 @@
-import { TUser } from '../types';
+import { TErrorMessage, TUser } from '../types';
 
 export interface IUserService {
   save: (data: TUser) => Promise<TUser>;
+}
+
+export interface IErrorHandler extends Error {
+  status?: number;
+  isJoi?: boolean;
+  details?: TErrorMessage[];
 }
