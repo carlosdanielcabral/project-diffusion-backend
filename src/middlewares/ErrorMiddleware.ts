eef2bdb40d33a3498992c56c0dd2cf284a59b569
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-20 21:24:24 -0300

    refactor: error middleware

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,14 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import ErrorHandler from '../helpers/ErrorHandler';
+import { IErrorHandler } from '../interfaces';
 
 const ErrorMiddleware = (
-  err: Error | ErrorHandler,
+  err: IErrorHandler,
   _req: Request,
   res: Response,
   _next: NextFunction,
 ) => {
   if (err instanceof ErrorHandler) {
     return res.status(err.status).json({ message: err.message });
+  }
+
+  if (err.isJoi) {
+    return res.status(400).json({ message: err.details[0].message });
   }
 
   return res.status(500).json({ message: 'Internal Server Error' });
