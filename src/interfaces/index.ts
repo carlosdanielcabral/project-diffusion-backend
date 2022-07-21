e7b95823f4898e4e4b57d2bffa35764a864ddd8a
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-21 11:36:07 -0300

    feat: add new methods to IUserService

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,11 @@
 import { TErrorMessage, TUser, TUserField } from '../types';
 
 export interface IUserService {
+  findAll: () => Promise<TUser[]>;
+  findAllByFilter: (
+    field: TUserField,
+    value: number | string,
+  ) => Promise<TUser[]>;
   findOne: (field: TUserField, value: number | string) => Promise<TUser>;
   login: (data: TUser) => Promise<TUser>;
   save: (data: TUser) => Promise<TUser>;
