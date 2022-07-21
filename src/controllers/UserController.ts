43360793dbf6833d20b2eeadf76f14f7af9972de
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-20 22:41:52 -0300

    feat: add method login to user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { SignOptions } from 'jsonwebtoken';
+import Token from '../auth/Token';
+import { JWT_CONFIG } from '../consts';
 import { IUserService } from '../interfaces';
 
 class UserController {
@@ -6,10 +9,24 @@ class UserController {
     this._service = _service;
   }
 
+  login = async (req: Request, res: Response, next: NextFunction) => {
+    const userData = req.body;
+    try {
+      const user = await this._service.login(userData);
+      const token = new Token(JWT_CONFIG as SignOptions);
+      const tokenValue = token.generate(user);
+
+      return res.status(200).json({ token: tokenValue });
+    } catch (err) {
+      return next(err);
+    }
+  };
+
   save = async (req: Request, res: Response, next: NextFunction) => {
     const userData = req.body;
     try {
       const user = await this._service.save(userData);
+
       return res.status(201).json(user);
     } catch (err) {
       return next(err);
