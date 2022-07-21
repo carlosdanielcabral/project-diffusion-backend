abfad04621c8d51f702b601361d9e18c825f5c59
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-21 20:45:12 -0300

    feat: add TPost and TPostField types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,31 @@ export type TErrorMessage = {
   message: string;
 };
 
+export type TPost = {
+  id?: number;
+  author?: number;
+  title?: string;
+  content?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  authorData?: {
+    id: number;
+    name: string;
+  };
+};
+
+export type TPostField =
+  | 'id'
+  | 'author'
+  | 'title'
+  | 'content'
+  | 'createdAt'
+  | 'updatedAt';
+
 export type TUser = {
   id?: number;
   name?: string;
-  email: string;
+  email?: string;
   password?: string;
 };
 
