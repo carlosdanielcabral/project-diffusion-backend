71a55e9fd98d660d1e6f0cb608515f4bdd6e8d3a
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-07-21 20:50:59 -0300

    feat: add PostRouter to RouteFactory

--- src/factories/RouteFactory.ts
+++ src/factories/RouteFactory.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import UserRouter from '../routes/UserRouter';
 import Route from '../routes';
 import UserFactory from './UserFactory';
+import PostRouter from '../routes/PostRouter';
+import PostFactory from './PostFactory';
 
 const RouteFactory = () => {
-  const controller = UserFactory();
-  const router = new UserRouter(Router(), controller);
-  const route = new Route(Router(), router);
+  const userController = UserFactory();
+  const postController = PostFactory();
+  const userRouter = new UserRouter(Router(), userController);
+  const postRouter = new PostRouter(Router(), postController);
+  const route = new Route(Router(), userRouter, postRouter);
   return route;
 };
 
